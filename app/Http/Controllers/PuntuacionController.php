4c5e06e18e04fb586be6af21724061ea5b9966d8
louis <l.c.m.p.28@gmail.com>
2020-11-18 12:26:10 -0600

    Controlador de puncuaciones

---
+++ app/Http/Controllers/PuntuacionController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Puntuacion;
+use Illuminate\Http\Request;
+
+class PuntuacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Puntuacion::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $puntuacion = new Puntuacion;
+
+        $puntuacion->puntuacion = $request->puntuacion;
+        $puntuacion->comentario = $request->comentario;
+        $puntuacion->proveedor_id = $request->proveedor_id;
+        $puntuacion->cliente_id = $request->cliente_id;
+
+        $puntuacion->save();
+
+        return $puntuacion;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
